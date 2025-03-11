79dfb8187c8002752441c75de1553e963584933d
Swetha53 <swethanarayan25@gmail.com>
2025-03-11 18:13:07 -0400

    changed repo for hosting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Swetha53.github.io",
+  base: "/NovaMart/",
 });
