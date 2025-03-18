2447449a71ce38e1700d29e0eb12a74ca5c7ac3f
Swetha53 <swethanarayan25@gmail.com>
2025-03-18 00:43:48 -0400

    project

--- src/pages/AR/AR.jsx
+++ src/pages/AR/AR.jsx
@@ -6,6 +6,8 @@ import "@google/model-viewer";
 const AR = () => {
   const { productId } = useParams();
   const modelUrl =
+    "https://reggvbnnkqmprlkojomx.supabase.co/storage/v1/object/public/Products//8989f64c-0ddc-4416-94f4-cd7cadc32131.glb";
+  const iosModel =
     "https://reggvbnnkqmprlkojomx.supabase.co/storage/v1/object/public/Products//object.usdz";
   return (
     <div className="ar">
@@ -13,11 +15,14 @@ const AR = () => {
         className="ar__model"
         id="product-demo"
         ar
+        ar-scale="fixed"
         ar-modes="webxr scene-viewer quick-look"
         camera-controls
         touch-action="pan-y"
         max-camera-orbit="auto 90deg auto"
         src={modelUrl}
+        ios-src={iosModel}
+        xr-environment
         alt="Product"
       >
         <button slot="ar-button" id="ar-button">
